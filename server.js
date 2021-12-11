aaa06055f29d12b7618d97368408ab3fdc8169a9
Hardik <hardikrajpal@cse.iitb.ac.in>
2021-12-11 14:35:39 +0530

    predep stages

---
+++ server.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/Educare'));
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/Educare/'}),
+);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
