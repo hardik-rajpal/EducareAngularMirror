21c19139367a58c8756eca664b527c06064ebaa6
Hardik <hardikrajpal@cse.iitb.ac.in>
2021-12-11 16:46:55 +0530

    educare-django->educare-dj

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  server_url:'https://educare-django.herokuapp.com'
+  server_url:'https://educare-dj.herokuapp.com'
   // server_url:'http://127.0.0.1:8000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // server_url:'https://educare-django.herokuapp.com'
-  server_url:'http://127.0.0.1:8000'
+  server_url:'https://educare-dj.herokuapp.com'
+  // server_url:'http://127.0.0.1:8000'
 };
 
 /*
